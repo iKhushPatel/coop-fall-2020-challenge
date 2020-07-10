2b7b476c99c6ced5d166b7635b33dc2a5be83b03
Khush Patel <ikhushpatel@gmail.com>
2020-07-10 14:34:29 -0400

    Update solution_python.py
    Name: Khushkumar Patel
    Co-op Fall 2020 
    Email ID : patel2a4@uwindsor.ca

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,52 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.operation = []
+        self.step = []
+        self.l_undo = []
 
     def add(self, num: int):
-        pass
+        self.value += num
+        self.operation.append(['sum', num])
+        self.step.append(['sum', num])
+        return self.value
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        self.operation.append(['sub', num])
+        self.step.append(['sub', num])
+        return self.value
 
     def undo(self):
-        pass
+        if(len(self.step) == 0):
+            return str(self.value)
+        self.lastop = self.operation.pop()
+        if (self.lastop[0] =='sum'):
+            self.value -= self.lastop[1]
+        else:
+            self.value += self.lastop[1]
+        self.l_undo.append('undo')
+        return self.value
 
     def redo(self):
-        pass
+        if(len(self.step) == 0):
+            return str(self.value)
+        if (self.lastop[0] =='sum'):
+            self.value += self.lastop[1]
+        else:
+            self.value -= self.lastop[1]
+        self.l_undo.pop()
+        return self.value
 
     def bulk_undo(self, steps: int):
-        pass
+        for i in range(steps):
+            self.undo()
+        return self.value    
 
     def bulk_redo(self, steps: int):
-        pass
+        if(self.l_undo.count('undo') < steps):
+            steps = self.l_undo.count('undo')
+        
+        for i in range(steps):
+            self.redo()
+        return self.value
